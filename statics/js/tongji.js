982fadd18181ee297bfa57a4c2e2d73067b902c4
zmuv188 <76840449+zmuv188@users.noreply.github.com>
2021-01-05 16:08:39 +0800

    Add files via upload

---
+++ statics/js/tongji.js
@@ -0,0 +1,30 @@
+var icen = $('body');
+
+var closeTips = function(){
+    $('.mtips').addClass('hide');
+}
+
+var son =$('.searchstart-on'),
+    soff=$('.searchstart-off'),
+    isea=$('.searchform');
+son.on('touchend',function(e){
+    $(this).hide();
+    isea.show();
+    soff.show();
+    e.preventDefault();
+})
+soff.on('touchend',function(e){
+    $(this).hide();
+    son.show();
+    isea.hide();
+    e.preventDefault();
+})
+
+﻿var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b6d73184c996643d25f956a9331d7592";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
