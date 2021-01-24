46a3f249e94aaa8e8c590380af0c78d9a5cdfc0a
zmuv188 <76840449+zmuv188@users.noreply.github.com>
2021-01-24 20:34:21 +0800

    Add files via upload

---
+++ statics/js/hgtj.js
@@ -0,0 +1,36 @@
+﻿	$(".sort-button").on("click", function() {
+		var orderBy = $(this).attr("data-order");
+		if (orderBy == "asc") {
+			$(this).attr("data-order", "desc");
+			$(this).html('<i class="icon icon-sort-by-order"></i>&nbsp;正序⇊')
+		} else {
+			$(this).attr("data-order", "asc");
+			$(this).html('<i class="icon icon-sort-by-order-alt"></i>&nbsp;倒序⇈')
+		}
+		var listWrap = $(this).parents(".stui-pannel-box").find("ul");
+		var elems = listWrap.children("li");
+		var arr = $.makeArray(elems);
+		arr.reverse();
+		$(arr).appendTo(listWrap)
+	})
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b6d73184c996643d25f956a9331d7592";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
