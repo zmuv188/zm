4babc787d0531843ce3a14f7ab0e0a42a5be4bda
zmuv188 <76840449+zmuv188@users.noreply.github.com>
2021-01-24 20:48:21 +0800

    Update 73tj.js

--- statics/js/73tj.js
+++ statics/js/73tj.js
@@ -2,10 +2,10 @@
 		var orderBy = $(this).attr("data-order");
 		if (orderBy == "asc") {
 			$(this).attr("data-order", "desc");
-			$(this).html('<i class="icon icon-sort-by-order"></i>&nbsp;正序⇊')
+			$(this).html('<i class="icon icon-sort-by-order"></i>&nbsp;正序⇅')
 		} else {
 			$(this).attr("data-order", "asc");
-			$(this).html('<i class="icon icon-sort-by-order-alt"></i>&nbsp;倒序⇈')
+			$(this).html('<i class="icon icon-sort-by-order-alt"></i>&nbsp;倒序⇅')
 		}
 		var listWrap = $(this).parents(".stui-pannel-box").find("ul");
 		var elems = listWrap.children("li");
