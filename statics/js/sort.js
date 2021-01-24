005ae876370b0794ec5c97ae33246a6d239cb27f
zmuv188 <76840449+zmuv188@users.noreply.github.com>
2021-01-24 19:38:19 +0800

    Add files via upload

---
+++ statics/js/sort.js
@@ -0,0 +1,15 @@
+	$(".sort-button").on("click", function() {
+		var orderBy = $(this).attr("data-order");
+		if (orderBy == "asc") {
+			$(this).attr("data-order", "desc");
+			$(this).html('<i class="icon icon-sort-by-order"></i>&nbsp;顺序')
+		} else {
+			$(this).attr("data-order", "asc");
+			$(this).html('<i class="icon icon-sort-by-order-alt"></i>&nbsp;倒序')
+		}
+		var listWrap = $(this).parents(".stui-pannel-box").find("ul");
+		var elems = listWrap.children("li");
+		var arr = $.makeArray(elems);
+		arr.reverse();
+		$(arr).appendTo(listWrap)
+	})
